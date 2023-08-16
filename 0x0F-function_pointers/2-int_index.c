cad7cf795140088f8334e8b855f63a0d344f2f24
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2023-08-16 21:25:38 +0000

    Function Pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,6 +3,13 @@
 /**
  * int_index - compare integers in an array to a function
  * given as parameter
+ * @array: the array to iterate through
+ * @size: the number of elements in the array
+ * @cmp: the pointer  to the function to be used to compare values
+ * Return: returns the index of the first element for
+ *  which the cmp function does not return 0
+ *  If no element matches, return -1
+ *  If size <= 0, return -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
